6fe850e8fc534f552aad2a05db58dd77122e613d
SaacK <161529566+SaacK12@users.noreply.github.com>
2025-06-11 20:51:24 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,14 +1,78 @@
 function CalcularPrecio(){
 let precio= 35
-let cantidad= parseInt(document.getElementById("Cantidad").value)
-let marca= document.getElementById("Marca")
-let preciodescuento= parseFloat(document.getElementById("precioDescuento").value)
-console.log(cantidad)
+let cantidad= parseInt(document.getElementById("Cantidad").value);;
+let marca= document.getElementById("Marca").value;
+let preciodescuento= 0
+let resultado= document.getElementById.value
 
-
+//NOTA: Remplaza los alert por el ID
 if(isNaN(cantidad) || cantidad <= 0){
     alert("Ingrese un numero valido o al menos 1 lampara")
+    return;
 }
-if ((cantidad > 0)  && (cantidad < 6)){
-preciodescuento= precio }
-}
+
+
+switch(cantidad){
+    //Item A
+    case 6: 
+        preciodescuento= parseInt(precio * cantidad * 0.5);
+        resultado= (precio * cantidad) - preciodescuento;
+        alert("Precio con descuento: "+resultado);
+        break;
+    //Item B
+    case 5:
+        switch(marca){
+            case "ArgentinaLuz":
+                preciodescuento= parseInt(precio * cantidad * 0.4);
+                resultado= (precio * cantidad) - preciodescuento
+                alert("Precio con descuento: "+resultado);
+                break;
+                default:
+        preciodescuento= parseInt(precio * cantidad * 0.3);
+        resultado= (precio * cantidad) - preciodescuento
+        alert("Precio con descuento: "+resultado);
+        break;
+         }
+         break;
+    //Item C
+    case 4:
+        switch(marca){
+            case "ArgentinaLuz": //Para arreglar ahora me da pereza, puedo en este caso puedo cambiar el switch por un if.
+            case "FelipeLamparas":
+                preciodescuento= parseInt(precio * cantidad * 0.25);
+                resultado= (precio * cantidad) - preciodescuento
+                alert("Precio con descuento: "+resultado);
+                break;
+            default:
+                preciodescuento= parseInt(precio * cantidad * 0.2);
+                resultado= (precio * cantidad) - preciodescuento
+                alert("Precio con descuento: "+resultado);
+                break;
+                }
+    //Item D
+    case 3:
+        switch(marca){
+            case "ArgentinaLuz":
+                preciodescuento= parseInt(precio * cantidad * 0.15);
+                resultado= (precio * cantidad) - preciodescuento
+                alert("Precio con descuento: "+resultado);
+                break;
+            case "FelipeLamparas":
+                preciodescuento= parseInt(precio * cantidad * 0.1);
+                resultado= (precio * cantidad) - preciodescuento
+                alert("Precio con descuento: "+resultado);
+                break;
+            default:
+                preciodescuento= parseInt(precio * cantidad * 0.05);
+                resultado= (precio * cantidad) - preciodescuento
+                alert("Precio con descuento: "+resultado);
+                break;
+        }
+
+         }
+          }
+         //175
+         //
+
+
+//35 * 5*  0,4
