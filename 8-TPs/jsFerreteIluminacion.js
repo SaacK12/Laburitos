4f3edbdb77bff7393d2b64d5fbf55e2e60eab91f
SaacK <161529566+SaacK12@users.noreply.github.com>
2025-06-12 12:41:30 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,51 +1,62 @@
+let resultado= 0;
+
 function CalcularPrecio(){
 let precio= 35
 let cantidad= parseInt(document.getElementById("Cantidad").value);;
 let marca= document.getElementById("Marca").value;
-let preciodescuento= 0
-let resultado= document.getElementById.value
+let preciodescuento= 0;
+let resultadoElemento= document.getElementById("precioDescuento");
+let ingresosBrutos= 0
 
-//NOTA: Remplaza los alert por el ID
+//Validar que la cantidad sea un numero y que no sea 0 o menos
 if(isNaN(cantidad) || cantidad <= 0){
     alert("Ingrese un numero valido o al menos 1 lampara")
     return;
 }
-
-switch(cantidad){
-    //Item A
-    case 6: 
-        preciodescuento= parseInt(precio * cantidad * 0.5);
+//Item A
+//Si se comprar 6 o MAS lamparas se evitara entrar al switch
+ if (cantidad >= 6){
+    preciodescuento= parseInt(precio * cantidad * 0.5);
         resultado= (precio * cantidad) - preciodescuento;
-        alert("Precio con descuento: "+resultado);
-        break;
+        //Si el precio esde 120 o mas
+        if(resultado >= 120){ 
+             impuesto()}
+        else{ 
+        resultadoElemento.value="$"+ resultado;
+         }
+        return;
+ }
+switch(cantidad){
     //Item B
     case 5:
         switch(marca){
             case "ArgentinaLuz":
                 preciodescuento= parseInt(precio * cantidad * 0.4);
                 resultado= (precio * cantidad) - preciodescuento
-                alert("Precio con descuento: "+resultado);
+                resultadoElemento.value="$"+ resultado;
                 break;
-                default:
-        preciodescuento= parseInt(precio * cantidad * 0.3);
-        resultado= (precio * cantidad) - preciodescuento
-        alert("Precio con descuento: "+resultado);
-        break;
+            default:
+                preciodescuento= parseInt(precio * cantidad * 0.3);
+                resultado= (precio * cantidad) - preciodescuento
+                resultadoElemento.value="$"+ resultado;
+                break;
          }
          break;
     //Item C
     case 4:
         switch(marca){
-            case "ArgentinaLuz": //Para arreglar ahora me da pereza, puedo en este caso puedo cambiar el switch por un if.
+            case "ArgentinaLuz":
             case "FelipeLamparas":
                 preciodescuento= parseInt(precio * cantidad * 0.25);
                 resultado= (precio * cantidad) - preciodescuento
-                alert("Precio con descuento: "+resultado);
-                break;
+                resultadoElemento.value="$"+ resultado;
+                return;
+                break; //Aca puse return antes del break, asi que no se si esta al pedo los break pero que mas da sale un balatro
             default:
                 preciodescuento= parseInt(precio * cantidad * 0.2);
                 resultado= (precio * cantidad) - preciodescuento
-                alert("Precio con descuento: "+resultado);
+                resultadoElemento.value="$"+ resultado;
+                return;
                 break;
                 }
     //Item D
@@ -54,19 +65,34 @@ switch(cantidad){
             case "ArgentinaLuz":
                 preciodescuento= parseInt(precio * cantidad * 0.15);
                 resultado= (precio * cantidad) - preciodescuento
-                alert("Precio con descuento: "+resultado);
+                resultadoElemento.value="$"+ resultado;
+                return;
                 break;
             case "FelipeLamparas":
                 preciodescuento= parseInt(precio * cantidad * 0.1);
                 resultado= (precio * cantidad) - preciodescuento
-                alert("Precio con descuento: "+resultado);
+                resultadoElemento.value="$"+ resultado;
+                return;
                 break;
             default:
                 preciodescuento= parseInt(precio * cantidad * 0.05);
                 resultado= (precio * cantidad) - preciodescuento
-                alert("Precio con descuento: "+resultado);
+                resultadoElemento.value="$"+ resultado;
+                return;
                 break;
         }
+    //Si no se cumple ningun item y no hay descuento
+    default:
+        resultado= precio * cantidad
+        resultadoElemento.value="$"+ resultado+(" (No hay descuento disponible)");
+        break;
          }
           }
-//35 * 5*  0,4
+//Sumarle impuesto
+function impuesto(){
+    let impuestoSumado = resultado * 0.10;
+    resultado = resultado + impuestoSumado;
+    alert("Usted pagó $" + impuestoSumado.toFixed(2) + " de IIBB.");
+    let resultadoElemento = document.getElementById("precioDescuento");
+    resultadoElemento.value = "$" + resultado.toFixed(2);
+}
